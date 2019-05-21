079949814787dfd928b324c1d2c4b029dbc73207
Nityesh Agarwal <nityeshagarwal@gmail.com>
2019-05-21 12:52:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,8 @@ set -e
 
 sbt clean compile assembly
 
-python -m awscli s3 cp target/scala-2.11/ingestion.jar s3://${BUCKET_NAME}/
+# Extract release version from build.sbt
+releaseVersion="$(grep "^val releaseVersion = \".\..\...\"" ./build.sbt | grep "\".*\"" -o | sed "s/\"//g")"
+
+# Upload jar to bucket
+python -m awscli s3 cp target/scala-2.11/ingestion.jar s3://${BUCKET_NAME}/share/lib/$(releaseVersion)/
